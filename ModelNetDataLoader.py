961c82837daa926cfddbaa34c240cada32068213
vccheng2001 <vccheng@andrew.cmu.edu>
2021-04-10 15:01:16 -0700

    Change rot matrix to 3D, in y dir

--- ModelNetDataLoader.py
+++ ModelNetDataLoader.py
@@ -8,48 +8,56 @@ import numpy as np
 import sys 
 
 
-class ModelNet40DataLoader(Dataset):
+class ModelNet40Dataset(Dataset):
 
-    def __init__(self, root, category, transform=True):
+    def __init__(self, root, category, rotate=True):
         # root directory 
         self.root = root
-        self.transform = transform
-        # airplane, bathtub, ..
+        # airplane, bathtub, bed, etc
         self.category = category 
-        # example: "data/modelnet40/airplane ""
-        self.path = os.listdir(root+category)
-
-
-    def __getitem__(self, index=None):
-        # if index is specified
-        if index:
-            file_name = self.path[index]
-        # else choose random file 
-        else:
-            file_name = random.choice(self.path)    
-        # original pointcloud
-        original = np.loadtxt(self.root+self.category+file_name, \
-                              delimiter=",", dtype=np.float64)
-        
-        print(f"Original pointcloud shape: {original.shape}")
-        if self.transform:
-            # randomly transform pointcloud
-            theta = np.random.uniform(0, 2*np.pi)
-            rot = np.array([[np.cos(theta), -np.sin(theta)],
-                            [np.sin(theta), np.cos(theta)]])
-            
-            transformed = np.dot(rot, original)
-
-            return (original, transformed)
+        # specify random rotation
+        self.rotate = rotate 
+        # example: "data/modelnet40/airplane/"
+        self.path = root+category
+        # files in path
+        self.data = os.listdir(self.path)
 
     def __len__(self):
         return len(self.path)
 
+    def __getitem__(self, index=None):
+        # if index is specified
+        if index:
+            file = self.data[index]
+        else:
+            file = np.random.choice(self.data) 
+        
+        # source pointcloud
+        src = np.loadtxt(self.path+file, delimiter=",", dtype=np.float64)
+
+        print(f"Source pointcloud shape: {src.shape}")
+        if self.rotate:
+            target = rotate(src)
+
+        src = torch.from_numpy(src)
+        target = torch.from_numpy(target)
+        return (src, target)
+
+
+def rotate(src):
+    # randomly rotate pointcloud in y dir 
+    theta = np.random.uniform(0, 2*np.pi)
+    rot = np.array([[np.cos(theta), 0, np.sin(theta)],
+                    [0,             1,         0    ],
+                    [-np.sin(theta),0, np.cos(theta)]])
+    return np.dot(rot, src.reshape((3,-1)))
+        
+
 if __name__ == "__main__":
     root = './data/modelnet40_normal_resampled/'
     category = 'airplane/'
-    data = ModelNet40DataLoader(root=root,category=category)
-    DataLoader = torch.utils.data.DataLoader(data, batch_size=16, shuffle=True)
+    data = ModelNet40Dataset(root=root,category=category)
+    DataLoader = torch.utils.data.DataLoader(data, batch_size=1, shuffle=True)
     for original, transformed in DataLoader:
         print(original.shape)
         print(transformed.shape)
