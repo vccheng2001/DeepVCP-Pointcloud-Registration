a5aca6e0b527d36b085c095fcf00f452ce1ad85a
zhrzhang <zherui.bill.zhang@gmail.com>
2021-04-24 00:19:58 -0400

    bug fixes and change in gitignore

--- weighting_layer.py
+++ weighting_layer.py
@@ -5,27 +5,27 @@ from torch.nn.modules.activation import ReLU, Softplus
 from torch.nn.modules.batchnorm import BatchNorm1d
 from torch.nn.modules.linear import Linear
 
-class WeightingLayer(nn.Module):
+class weighting_layer(nn.Module):
     def __init__(self):
-        super(WeightingLayer, self).__init__()
+        super(weighting_layer, self).__init__()
         self.fc1 = nn.Sequential(
-            nn.Linear(32, 16, True)
-            nn.BatchNorm1d(16)
+            nn.Linear(32, 16, True),
+            # nn.BatchNorm1d(10000),
             nn.ReLU()
         )
         self.fc2 = nn.Sequential(
-            nn.Linear(16, 8, True)
-            nn.BatchNorm1d(8)
+            nn.Linear(16, 8, True),
+            # nn.BatchNorm1d(8),
             nn.ReLU()
         )
         self.fc3 = nn.Sequential(
-            nn.Linear(8, 1, True)
+            nn.Linear(8, 1, True),
             nn.Softplus()
         )
     
-    def forward(self, X, K = 512):
+    def forward(self, X, K = 64):
         X = self.fc1(X)
         X = self.fc2(X)
         X = self.fc3(X)
-
-        return torch.topk(X, K, dim = 2)
+    
+        return torch.topk(X, K, dim = 1).values
