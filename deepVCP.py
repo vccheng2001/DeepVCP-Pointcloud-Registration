8886f873aa5d6939462c3ccf52356de89311c67c
zhrzhang <zherui.bill.zhang@gmail.com>
2021-04-26 00:27:02 -0400

    added grouping after weighting layer

--- deepVCP.py
+++ deepVCP.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+from pointnet2_utils import sample_and_group
 from deep_feat_extraction import feat_extraction_layer
 from weighting_layer import weighting_layer
 
@@ -10,11 +11,12 @@ class DeepVCP(nn.Module):
         self.FE1 = feat_extraction_layer()
         self.WL = weighting_layer()
     
-    def forward(self, pts):
-        print(pts.shape)
-        deep_feat_xyz, deep_feat_pts = self.FE1(pts)
-        keypts_idx = self.WL(deep_feat_pts)
-        keypts = pts[:, :, keypts_idx]
+    def forward(self, src_pts, tgt_pts):
+        src_deep_feat_xyz, src_deep_feat_pts = self.FE1(src_pts)
+        src_keypts_idx = self.WL(src_deep_feat_pts)
+        src_keypts = src_pts[:, :, src_keypts_idx]
+        src_keypts = src_keypts.permute(0, 2, 1)
 
-        keypts = keypts.permute(0, 2, 1)
-        return keypts
+        src_keypts_grouped_xyz, src_keypts_grouped_pts = sample_and_group(npoint = 64, radius = 1, nsample = 32, xyz = src_keypts[:, :, :3], points = src_keypts[:, :, 3:])
+        
+        return src_keypts
