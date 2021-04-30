3f8a6f3f5f4783d433755fcf16b9954d435f7e9b
zhrzhang <zherui.bill.zhang@gmail.com>
2021-04-30 15:23:27 -0400

    finished k nearest neighbor and normalization

--- deepVCP.py
+++ deepVCP.py
@@ -24,10 +24,10 @@ class DeepVCP(nn.Module):
         # group the keypoints
         src_keypts_grouped_xyz, src_keypts_grouped_pts = sample_and_group(npoint = 64, radius = 1, nsample = 32, xyz = src_keypts[:, :, :3], points = src_keypts[:, :, 3:])
         
-        
         tgt_pts_xyz = tgt_pts[:, :3, :]
         tgt_pts_xyz = tgt_pts_xyz.permute(0, 2, 1)
         tgt_deep_feat_xyz, tgt_deep_feat_pts = self.FE1(tgt_pts)
+        print("tgt_deep_feat_pts: ", tgt_deep_feat_pts.shape)
         # get candidate points for corresponding points of the keypts in src
         r = 2.0
         s = 0.4
@@ -36,7 +36,7 @@ class DeepVCP(nn.Module):
         ###########################
         # candidate_pts = voxelize(src_keypts, r, s)
         candidate_pts = torch.randn(B, src_keypts.shape[1], 552, 3)
-        # reshape the candidate_pts from B * N * C * 3 to B * (N * C) * 3 to perform knn
+        # reshape the candidate_pts from (B, N, C, 3) to (B, (N * C), 3) to perform knn
         candidate_pts_flat = torch.flatten(candidate_pts, start_dim = 1, end_dim = 2)
         
         # use KNN to find nearest neighbors of the candidates in tgt_pts 
@@ -49,8 +49,29 @@ class DeepVCP(nn.Module):
         # normalize the deep features based on distance
         dist_sum = torch.sum(dist, dim = 2, keepdim = True, dtype = float)
         dist_normalize = dist / dist_sum
-        print(dist_normalize.shape)
-        feat_weight_map = (dist_normalize.repeat(1, 1, k_nn)).repeat(B, 1, 1, 1)
+
+        # stacking normalized distance into a map for deep features
+        # feat_weight_map: (B, N * C, K, C_deep_feat) C_deep_feat is number of channels for deep features 
+        feat_weight_map = dist_normalize.unsqueeze(3).repeat(1, 1, 1, k_nn)
+        print("feat_weight_map: ", feat_weight_map.shape)
+
+        # pick deep features of tgt_pts with idx
+        N_keypts = src_keypts.shape[1]
+        C_candidates = candidate_pts.shape[2]
+        C_deep_feat = tgt_deep_feat_pts.shape[2]
+        N_idx = idx.shape[1]
+        C_idx = idx.shape[2]
+        idx_1_mask = torch.arange(B)
+        idx_1_mask = idx_1_mask.unsqueeze(1).repeat(1, B)
+        idx_1_mask = idx_1_mask.flatten()
+        idx_2_mask = idx.flatten()
+        tgt_feat_picked = tgt_deep_feat_pts[idx_1_mask, idx_2_mask, :].view(B, N_keypts, C_candidates, k_nn, C_deep_feat)
+        
+        # reshape the normalizing feature weight map
+        feat_weight_map = feat_weight_map.view(B, N_keypts, C_candidates, k_nn, C_deep_feat)
+
+        # normalize the picked deep features from tgt_pts
+        tgt_feat_norm = tgt_feat_picked * feat_weight_map
         
 
         # obtain the top k indices for tgt point clouds
