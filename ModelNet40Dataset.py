fixed a bug in gt normal calculation in dataloader

ModelNet40Dataset.py
@@ -78,7 +78,7 @@ class ModelNet40Dataset(Dataset):
 
             # rotate source point cloud and normals
             target_points = R @ src_points
-            target_normal = R @ target_points
+            target_normal = R @ src_normals
 
         src_points = torch.from_numpy(src_points) + t
         src_normals = torch.from_numpy(src_normals)

deepVCP.py
@@ -94,4 +94,4 @@ class DeepVCP(nn.Module):
         tgt_vcp = self.cpg(src_dfe_feat, tgt_dfe_feat, candidate_pts)
         print("vcp: ", tgt_vcp.shape)
 
-        return src_keypts, tgt_vcp
+        return src_keypts[:, :, :3], tgt_vcp

deepVCP_loss.py
@@ -54,7 +54,7 @@ def get_rigid_transform(x, y):
 @return R2: Bx3x3 calculated rotation matrix 
         t2: Bx3xN calculated translation
 '''
-def svd_optimization(x, y_true, R_true, t_true):
+def svd_optimization(x, y_pred, R_true, t_true):
 
     # ground truth y: Bx3xN
     y_true = torch.matmul(R_true, x) + t_true
@@ -100,13 +100,16 @@ Combine L1 loss function with
 '''
 
 def deepVCP_loss(x, y_pred, R_true, t_true, alpha):
+    x = x.permute(0, 2, 1)
     # l1 loss
     loss1 = nn.L1Loss(reduction="mean") 
 
     # svd loss
+    print('R_true: ', R_true.shape)
+    print('x', x.shape)
     R, t = svd_optimization(x, y_pred, R_true, t_true)
     print(f'Final Rotation: {R}')    
-    print(f'Final Translation: {t}') 
+    print(f'Final Translation: {t}')
     y_true = torch.matmul(R_true, x) + t_true
     loss2 = torch.mean(torch.sub(y_pred, y_true))
 

train.py
@@ -12,7 +12,6 @@ from ModelNet40Dataset import ModelNet40Dataset
 from utils import *
 
 from deepVCP_loss import deepVCP_loss
-from deep_feat_extraction import feat_extraction_layer
 
 ''' note: path to dataset is ./data/modelnet40_normal_resampled
     from https://modelnet.cs.princeton.edu/ '''
@@ -41,7 +40,8 @@ def main():
     # Load ModelNet40 data 
     print('Loading Model40 dataset ...')
 
-    root = '/home/zheruiz/datasets/modelnet40_normal_resampled/'
+    # root = '/home/zheruiz/datasets/modelnet40_normal_resampled/'
+    root = 'data/modelnet40_normal_resampled/'
     # only use airplane for now
     # shape_names = np.loadtxt(root+"modelnet40_shape_names.txt", dtype="str")
 